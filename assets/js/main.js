9250523fd33f8dd2e06ce82dd1130f62fe3d67e1
Kaichi X <kcxie@ucdavis.edu>
2022-12-05 01:08:58 +0800

    Cleaned all and Added Text

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,4 +54,4 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400});
 sr.reveal('.home__social-icon',{ interval: 200});
 sr.reveal('.skills__data, .work__img, .contact__input, .work__RE',{interval: 200});
-sr.reveal('.miscellenous__container, .personal',{interval: 200});
+sr.reveal('.miscellaneous__container, .personal',{interval: 200});
